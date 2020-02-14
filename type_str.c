647595f47b3ca828229ce271cff39b27c968a8ba
Kate Senaida <ksenaida@at-m6.21-school.ru>
2020-02-14 20:30:38 +0300

    g

--- type_str.c
+++ type_str.c
@@ -6,7 +6,7 @@
 /*   By: ksenaida <ksenaida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/23 14:34:15 by ksenaida          #+#    #+#             */
-/*   Updated: 2020/02/14 20:22:52 by ksenaida         ###   ########.fr       */
+/*   Updated: 2020/02/14 20:28:39 by ksenaida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,42 +57,40 @@ void	type_s(t_printf *list)
 	if (str == 0)
 		//ft_putstr_cow("(null)", list);
 		str = "(null)";
-	else
+
+	if (list->width >= ft_strlen(str))
+		list->widthofline = list->width;
+	else if (list->width < ft_strlen(str) && list->nw == 'n' && \
+			list->precision < ft_strlen(str) && list->np == 'n')
+		list->widthofline = list->width;
+	else if (list->width < ft_strlen(str) && list->nw == 'n')
 	{
-		if (list->width >= ft_strlen(str))
-			list->widthofline = list->width;
-		else if (list->width < ft_strlen(str) && list->nw == 'n' && \
-				list->precision < ft_strlen(str) && list->np == 'n')
-			list->widthofline = list->width;
-		else if (list->width < ft_strlen(str) && list->nw == 'n')
-		{
-			list->widthofline = ft_strlen(str);
-			/*
-			if (list->precision < ft_strlen(str) && list->precision > 0)
-				list->widthofline = list->precision;
-			else
-				list->widthofline = ft_strlen(str);
-			*/
-		}
-		else if (list->precision < ft_strlen(str) && list->np == 'n')
+		list->widthofline = ft_strlen(str);
+		/*
+		if (list->precision < ft_strlen(str) && list->precision > 0)
 			list->widthofline = list->precision;
 		else
 			list->widthofline = ft_strlen(str);
-		
-		if (list->nw == 'y' && list->precision == 0 && list->np == 'n')
-		{
-			list->widthofcontent = 0;
-			list->widthofline = 0;
-		}
-		else if (list->precision == 0 && list->np == 'n')
-			list->widthofcontent = 0;
-		else if (list->precision < ft_strlen(str) && list->np == 'n')
-			list->widthofcontent = list->precision;
-		else 
-			list->widthofcontent = ft_strlen(str);
-		if ((list->flag == '-') && (list->width > list->widthofcontent))
-			str_print_with_minus(list, str);
-		else
-			str_print_without_minus(list, str);
+		*/
 	}
+	else if (list->precision < ft_strlen(str) && list->np == 'n')
+		list->widthofline = list->precision;
+	else
+		list->widthofline = ft_strlen(str);
+	
+	if (list->nw == 'y' && list->precision == 0 && list->np == 'n')
+	{
+		list->widthofcontent = 0;
+		list->widthofline = 0;
+	}
+	else if (list->precision == 0 && list->np == 'n')
+		list->widthofcontent = 0;
+	else if (list->precision < ft_strlen(str) && list->np == 'n')
+		list->widthofcontent = list->precision;
+	else 
+		list->widthofcontent = ft_strlen(str);
+	if ((list->flag == '-') && (list->width > list->widthofcontent))
+		str_print_with_minus(list, str);
+	else
+		str_print_without_minus(list, str);
 }
