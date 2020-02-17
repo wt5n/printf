e8f919a85d2e3933f32afce323d94628058ead1d
Kate Senaida <ksenaida@at-n7.21-school.ru>
2020-02-17 16:35:20 +0300

    g

--- type_u.c
+++ type_u.c
@@ -2,14 +2,29 @@
 
 void	u_print_with_minus(t_printf *list, long long x)
 {
-	while (list->widthofline > 0)
+	if ((int)x < 0)
 	{
-		while (list->widthofcontent)
-		{
-			ft_putstr_cow(ft_itoa(x), list);
-			list->widthofline -= ft_len_of_int(x);
-			list->widthofcontent -= ft_len_of_int(x);
-		}
+		ft_putchar_cow('-', list);
+		x *= - 1;
+		if (list->precision > list->len_of_x)
+			list->widthofline--;
+	}
+	if (list->len_of_x > 0)
+		list->len_of_x = ft_len_of_int(x);
+	
+	while (list->precision > list->len_of_x)
+	{
+		ft_putchar_cow('0', list);
+		list->precision--;
+	}
+	if (list->len_of_x > 0)
+	{
+		ft_putstr_cow(ft_itoa(x), list);
+		list->widthofline -= list->len_of_x;
+		list->widthofcontent -= list->len_of_x;
+	}
+	while (list->widthofline > list->widthofcontent)
+	{
 		ft_putchar_cow(' ', list);
 		list->widthofline--;
 	}
@@ -17,25 +32,32 @@ void	u_print_with_minus(t_printf *list, long long x)
 
 void	u_presicion_over_len(t_printf *list, long long x)
 {
-	if ((int)x < 0)
+	while (list->widthofline > list->widthofcontent)
 	{
-		ft_putchar_cow('-', list);
-		x *= -1;
+		ft_putchar_cow(' ', list);
 		list->widthofline--;
 	}
-	while (list->widthofcontent > list->widthofline)
+	while (list->widthofcontent > list->len_of_x)
 	{
 		ft_putchar_cow('0', list);
 		list->widthofcontent--;
 	}
-	ft_putstr_cow(ft_itoa(x), list);
+	if (list->len_of_x > 0)
+		ft_putstr_cow(ft_itoa(x), list);
 }
 
 void	u_print_without_minus(t_printf *list, long long x)
 {
+	while (list->widthofline > list->widthofcontent && \
+		((list->precision < list->len_of_x && list->np == 'n') || \
+		(list->flag != '0' && list->flag2 != '0')))
+	{
+		ft_putchar_cow(' ', list);
+		list->widthofline--;
+	}
 	while (list->widthofline > list->widthofcontent)
 	{
-		if (list->flag == '0')
+		if (list->flag == '0' || list->flag2 == '0')
 		{
 			if ((int)x < 0)
 			{
@@ -50,7 +72,8 @@ void	u_print_without_minus(t_printf *list, long long x)
 			ft_putchar_cow(' ', list);
 		list->widthofline--;
 	}
-	ft_putstr_cow(ft_itoa(x), list);
+	if (list->len_of_x > 0)
+		ft_putstr_cow(ft_itoa(x), list);
 }
 
 void	type_u(t_printf *list)
@@ -58,28 +81,32 @@ void	type_u(t_printf *list)
 	long long x;
 
 	x = va_arg(list->ap, long long);
+	list->len_of_x = ft_len_of_int(x);
+	if (list->precision == 0 && list->np == 'n')
+		list->len_of_x = 0;
+
 	if (x < 0)
 		ft_putstr_cow("4294967269", list);
+
+	if (list->width > list->len_of_x)
+		list->widthofline = list->width;
 	else
 	{
-		if (list->width > ft_len_of_int(x))
-			list->widthofline = list->width;
+		if (list->precision < list->len_of_x && list->precision > 0)
+			list->widthofline = list->precision;
 		else
-		{
-			if (list->precision < ft_len_of_int(x) && list->precision > 0)
-				list->widthofline = list->precision;
-			else
-				list->widthofline = ft_len_of_int(x);
-		}
-		if (list->precision < ft_len_of_int(x))
-			list->widthofcontent = ft_len_of_int(x);
-		else
-			list->widthofcontent = list->precision;
-		if (list->precision > ft_len_of_int(x) - 1)
-			u_presicion_over_len(list, x);
-		else if ((list->flag == '-') && (list->width > list->widthofcontent))
-			u_print_with_minus(list, x);
-		else
-			u_print_without_minus(list, x);
+			list->widthofline = list->len_of_x;
 	}
+
+	if (list->precision < list->len_of_x)
+		list->widthofcontent = list->len_of_x;
+	else
+		list->widthofcontent = list->precision;
+	
+	if ((list->flag == '-') && (list->width > list->widthofcontent))
+		u_print_with_minus(list, x);	
+	else if (list->precision > list->len_of_x - 1)
+		u_presicion_over_len(list, x);
+	else
+		u_print_without_minus(list, x);
 }
